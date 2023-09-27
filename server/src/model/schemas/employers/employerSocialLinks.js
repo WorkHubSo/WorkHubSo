dd357c9121e7b7824b866a71d2c7320b933b5872
Miirshe <miirshe@gmail.com>
2023-09-27 09:07:41 +0300

    currently working employers in a backend side

---
+++ server/src/model/schemas/employers/employerSocialLinks.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose'
+const employerSocialLinksSchema = mongoose.Schema({
+    facebook: {
+        type: String,
+        required: false
+    },
+    twitter: {
+        type: String,
+        required: false
+    },
+    instagram: {
+        type: String,
+        required: false
+    },
+    linkedIn: {
+        type: String,
+        required: false
+    },
+    employerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'companyAccountSchema'
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now()
+    }
+
+})
+
+export const employerSocialLinksModel = mongoose.model('employerSocialLinksSchema', employerSocialLinksSchema)
